2eae9fd5fec5b1e7a02030f4440d81a2e515279a
mineunyoung <bear0369@naver.com>
2022-07-14 13:27:58 +0900

    feat: update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/button';
 import { Basic } from '@/layouts/Basic';
 import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
@@ -7,12 +8,31 @@ const Index = () => {
     <Main
       meta={
         <Meta
-          title="Next.js Boilerplate Presentation"
-          description="Next js Boilerplate is the perfect starter code for your project. Build your React application with the Next.js framework."
+          title="Poppymail"
+          description="📬 mail platform where letters arrive on random dates 'Poppymail'"
         />
       }
     >
-      <Basic title={'파피메일'}></Basic>
+      <Basic title={'파피메일'}>
+        <div className="flex h-96 w-full flex-col items-center justify-center">
+          <img
+            className="mb-4 w-64"
+            src="/assets/images/index/main.svg"
+            alt="poppy&felix"
+          />
+          <div className="text-center font-['Cafe24SsurroundAir'] text-lg">
+            오 안녕!
+            <br />
+            반가워
+          </div>
+        </div>
+        <div className="mx-8">
+          <div className="mb-2">
+            <Button type="primary">파피의 이야기 듣기</Button>
+          </div>
+          <Button type="secondary">로그인 / 회원가입하기</Button>
+        </div>
+      </Basic>
     </Main>
   );
 };
